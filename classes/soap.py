fc3a20bce928a0aea3caf8665b5da9a1b5cacfcc
maubarrerag <mauricio.barrerag@gmail.com>
2019-10-31 22:30:54 -0600

    Adding logging

--- classes/soap.py
+++ classes/soap.py
@@ -28,7 +28,6 @@ class Soap(object):
                     code = 600
                 else:
                     id = db.insert_telcel_trans(params)
-                    print(id)
                     if id > 0:
                         rec = db.select_telcel(id)
                         code = db.insert_telcel_trans(rec)
